2d749cf0a1fa4e488f1fdc298bbe33e01d7397a5
joshuaholloway <jhollow6@asu.edu>
2019-06-18 17:05:53 -0500

    end of day

--- tv_shows/apps/tv_shows_app/urls.py
+++ tv_shows/apps/tv_shows_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index),
+  url(r'^shows$', views.shows), # /shows
+  url(r'^new$', views.new),  # /new
+  url(r'^shows/(?P<show_id>\d+)$', views.show) # localhost:8000/shows/23
+
 ]
--- tv_shows/apps/tv_shows_app/views.py
+++ tv_shows/apps/tv_shows_app/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Shows
-
+# ======================================================================================================================
 def index(request):
-
-  context = {"shows": Shows.objects.all()}
+  return redirect("/shows")
+# ======================================================================================================================
+def shows(request):
+  shows = Shows.objects.all()
+  context = {"shows": shows}
   return render(request, "tv_shows_app/index.html", context)
+# ======================================================================================================================
+def show(request, show_id):
+  show = Shows.objects.get(id=show_id)
+  context = {"show": show}
+  return render(request, "tv_shows_app/show.html", context)
+# ======================================================================================================================
+def new(request):
+
+  # TODO: DO the query to create a new show
+
+  # TODO: Get the id of the new show
+
+  return render(request, "tv_show_app/show.html")
