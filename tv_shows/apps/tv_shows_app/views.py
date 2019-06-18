58beb72cc1b3b7d1c0e6a29cb15c801e43d809cc
joshuaholloway <jhollow6@asu.edu>
2019-06-18 11:20:02 -0500

    displaying table with entries from the database

--- tv_shows/apps/tv_shows_app/views.py
+++ tv_shows/apps/tv_shows_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import TV_show
 
 def index(request):
-  return HttpResponse("Hello")
+
+  context = {"shows": TV_show.objects.all()}
+  return render(request, "tv_shows_app/index.html", context)
